7234f9d3172221694a69f1489d09974e1619159e
iMac <imac@iMac-de-iMac.local>
2025-03-13 00:48:21 -0300

    se ajusta logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,34 @@ window.onscroll = function() {
     navbar.classList.remove('scrolled');
   }
 };
+
+
+let text = "Soy Gustavo";
+let i = 0;
+let isDeleting = false; 
+let speed = 150; // Velocidad de tipeo
+let delayBeforeReverse = 1000; // Pausa antes de borrar
+let minChars = 1; // Mínimo de caracteres antes de reiniciar
+let typingText = document.getElementById("typing-text");
+
+function typeEffect() {
+    typingText.textContent = text.substring(0, i); // Mantiene el bloque sin saltos
+
+    if (!isDeleting && i < text.length) {
+        i++;
+        setTimeout(typeEffect, speed);
+    } else if (!isDeleting && i === text.length) {
+        setTimeout(() => {
+            isDeleting = true;
+            typeEffect();
+        }, delayBeforeReverse);
+    } else if (isDeleting && i > minChars) {
+        i--;
+        setTimeout(typeEffect, speed);
+    } else {
+        isDeleting = false;
+        setTimeout(typeEffect, speed);
+    }
+}
+
+typeEffect();
